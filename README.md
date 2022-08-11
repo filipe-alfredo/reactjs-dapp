d416fdfec22ee10653c5682b4986654f1083db0c
Filipe Alfredo <filipealfredo.dev@gmail.com>
2022-08-11 15:19:30 -0300

    Initial version

--- README.md
+++ README.md
@@ -1,70 +1,72 @@
-# Getting Started with Create React App
+# Desenvolvendo uma Aplicação Descentralizada (dapp) com ReactJS
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Simulando um Blockchain RPC node
 
-## Available Scripts
+#### Instale o ganache-cli
+```console
+npm install -g ganache-cli
+```
 
-In the project directory, you can run:
+#### Inicie o ganache RPC de testes
+```console
+ganache-cli
+```
 
-### `npm start`
+---
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Criando um front-end com ReactJS
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+#### Crie uma aplicação ReactJS
+```console
+npx create-react-app reactjs-dapp
+```
 
-### `npm test`
+#### Instale as dependências necessárias
+```console
+cd reactjs-dapp
+npm install
+npm remove react-scripts && npm i react-scripts@4.0.3
+npm install web3
+```
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+---
 
-### `npm run build`
+## Criando um ambiente de desenvolvimento de Smart Contracts
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+#### Instale o truffle suite
+```console
+npm install -g truffle
+```
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+#### Inicie um projeto truffle
+```console
+truffle init truffle
+```
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+#### Instale as dependências necessárias
+```console
+cd truffle
+npm install
+npm install web3-utils
+npm install @openzeppelin/contracts
+```
 
-### `npm run eject`
+---
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+## Comandos úteis
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+#### Compile e realize o deploy dos Smart Contracts
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+##### Dentro da pasta truffle execute o comando abaixo:
+```console
+truffle migrate --reset --compile-all
+```
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+#### Inicie a aplicação ReactJS
 
-## Learn More
+##### Dentro da pasta reactjs-dapp execute o comando abaixo:
+```console
+npm start
+```
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+---
--- src/App.js
+++ src/App.js
@@ -1,23 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Web3 from 'web3'
+import ERC20ABI from './ERC20.json'
 
 function App() {
+
+  // Variáveis de estado do componente
+  const [defaultAccount, setDefaultAccount] = useState('');
+  const [tokenAddress, setTokenAddress] = useState('');
+  const [tokenName, setTokenName] = useState('');
+  const [tokenSymbol, setTokenSymbol] = useState('');
+  const [tokenSupply, setTokenSupply] = useState('');
+  const [tokenBalance, setTokenBalance] = useState('');
+  const [tokenTo, setTokenTo] = useState('');
+  const [tokenAmount, setTokenAmount] = useState('');
+
+  // Criar o objeto web3 com o provider metamask
+  const loadMetamask = () => {
+
+    // Verificar se o metamask esta instalado
+    if (window.ethereum && window.ethereum.isMetaMask) {
+
+      // Solicitar carteira selecionada
+      window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then(result => {
+          const wallet = result[0]
+
+          // Guardar o endereço da carteira
+          setDefaultAccount(wallet)
+          window.web3 = new Web3(window.ethereum)
+        })
+        .catch(error => {
+          console.log(error.message);
+        })
+    } else {
+      console.log("Metamask nao encontrado")
+    }
+  }
+
+  // Disparos reativos
+  useEffect(() => {
+
+    // Verifica se o Metamask esta instalado
+    if (window.ethereum && window.ethereum.isMetaMask) {
+
+      // Carrega os dados quando a rede for alterada
+      window.ethereum.on('chainChanged', async () => {
+        await loadMetamask()
+      })
+
+      // Carrega os dados quando a conta/carteira for alterada
+      window.ethereum.on('accountsChanged', async () => {
+        await loadMetamask()
+      })
+
+      // Carrega os dados do token
+      if(tokenAddress !== '') {
+        tokenAddressChange({ target: { value: tokenAddress } })
+      }
+    }
+  })
+
+  // Carrega os dados do token ERC20 através do endereço
+  const tokenAddressChange = async (evt) => {
+    try {
+
+      // Guarda todos os dados do token nas variáveis
+      const address = evt.target.value
+      let tokenContract = new window.web3.eth.Contract(ERC20ABI, address)
+      setTokenName(await tokenContract.methods.name().call())
+      setTokenSymbol(await tokenContract.methods.symbol().call())
+      setTokenSupply(window.web3.utils.fromWei(await tokenContract.methods.totalSupply().call()))
+      setTokenBalance(window.web3.utils.fromWei(await tokenContract.methods.balanceOf(defaultAccount).call()))
+      setTokenAddress(address)
+
+    } catch (err) {
+
+      // Limpa todos os dados do token nas variáveis
+      setTokenName(null)
+      setTokenSymbol(null)
+      setTokenSupply(null)
+      setTokenBalance(null)
+      console.log(evt, err.message)
+    }
+  }
+
+  // Guarda valor da carteira destino
+  const tokenToChange = async (evt) => {
+    setTokenTo(evt.target.value)
+  }
+
+  // Guarda valor a ser transferido para a carteira destino
+  const tokenAmountChange = async (evt) => {
+    setTokenAmount(evt.target.value)
+  }
+
+  // Envia tokens ERC20 para outra carteira
+  const send = async () => {
+
+    // Cria instancia do contrato
+    var tokenContract = new window.web3.eth.Contract(ERC20ABI, tokenAddress)
+
+    // Executa o metodo de transferencia
+    tokenContract.methods.transfer(tokenTo, window.web3.utils.toWei(tokenAmount))
+      .send({
+        from: defaultAccount
+    }).then(receipt => {
+      console.log(receipt)
+
+      // Atualiza o saldo
+      tokenContract.methods.balanceOf(defaultAccount).call().then(result => {
+        setTokenBalance(window.web3.utils.fromWei(result))
+      })
+    })
+  }
+  
+  // Renderização do template HTML
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> My First Application </h2>
+      <button className="btn" onClick={loadMetamask}>conectar</button>
+      <br />
+      <h4>Wallet: {defaultAccount}</h4>
+      <br />
+      <div>
+        <label>Contract Address:</label>
+        <input type="text" value={tokenAddress || ''} onChange={evt => tokenAddressChange(evt)} />
+        <br />
+        Name: {tokenName} ({tokenSymbol})
+        <br />
+        Supply: {tokenSupply}
+        <br />
+        Balance: {tokenBalance}
+      </div>
+      <br />
+      <label>To:</label>
+      <input type="text" value={tokenTo || ''} onChange={evt => tokenToChange(evt)} />
+      <br />
+      <label>Amount:</label>
+      <input type="number" value={tokenAmount || ''} onChange={evt => tokenAmountChange(evt)} />
+      <br />
+      <button onClick={send}>Enviar</button>
+      <br /><br />
     </div>
   );
 }
