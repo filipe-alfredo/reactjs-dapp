7a2fd0298bc2846477dc8d02f961e2eaf52269c6
Filipe Alfredo <filipealfredo.dev@gmail.com>
2022-08-12 00:47:49 -0300

    fix hello world contract

---
+++ src/truffle/contracts/HelloWorld.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+
+    string private name = "";
+
+    function hello() public view returns (string memory) {
+        return string(abi.encodePacked("Hello ", name));
+    }
+
+    function whatIsYourName(string memory newName) public {
+        name = newName;
+    }
+}
